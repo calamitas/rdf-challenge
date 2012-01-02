2fa914cdfdee4fd0055d19530a89d3b9187abd2c
Peter Vanbroekhoven <peter@xaop.com>
2012-01-02 12:37:43 +0100

    Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RdfChallenge::Application.config.secret_token = 'a9f5858b88ca8695dbbc1141aeeb8ba4f4f6658f20db4ea1326966c3b64aa090a1179a35ffcb60e41c14703cd1dab4158e0326667c91e950e6314c4c33b40d8f'
